b3bc751bc98b232298a494f0388ab128e4b48c9d
“Gustavo <gustavo.picoli@hotmail.com>
2025-07-23 20:37:36 -0300

    Versão Oficial do Projeto

---
+++ src/projetoteste/ProjetoTeste.java
@@ -0,0 +1,18 @@
+
+package projetoteste;
+
+/**
+ *
+ * @author Gustavo
+ */
+public class ProjetoTeste {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Essa é a versão oficial!");
+    }
+    
+}
